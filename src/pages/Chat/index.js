9a75ab5d4a5e58b9fc36e40ff29b090f59134127
LeonardoDM <leonardodiasmoraes@hotmail.com>
2021-01-22 00:33:11 -0300

    chat interface changes

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -4,48 +4,47 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import {Button, Container, Col, FormControl, InputGroup, Navbar, Row} from 'react-bootstrap'
 
 export default function Chat() {
+	
 	return (
 		<>
-			<Navbar>
-			    <Navbar.Brand>
-			      <img
-			        alt=""
-			        src="/chat-4-128.png"
-			        width="35"
-			        height="35"
-			        className="d-inline-block align-top"
-			      />{' '}
-			      Chat App
-			    </Navbar.Brand>
+			<Navbar fixed="top" className="mb-2">
+			    <Container>
+				    <Navbar.Brand>
+				      <img
+				        alt=""
+				        src="/chat-4-128.png"
+				        width="35"
+				        height="35"
+				        className="d-inline-block align-top"
+				      />{' '}
+				      Chat App
+				    </Navbar.Brand>
+				</Container>
 			</Navbar>
-			<Container fluid>
+			<Container className="container-height">
 				<Row>
-					<Col sm={4}>Users Online</Col>
-					<Col sm={8}>
-						<Row>
-							<Col xl={12}>
-								<div className="overflow-auto chat-area">
-								text
-								</div>
-							</Col>
-							<Col className="px-0">
-								<Navbar>
-								    <InputGroup className="my-2">
-										<FormControl
-									      placeholder="Type your message..."
-									      aria-label="Type your message..."
-									      aria-describedby="basic-addon2"
-									    />
-									    <InputGroup.Append>
-									      <Button variant="outline-success">></Button>
-									    </InputGroup.Append>
-									</InputGroup>
-								</Navbar>
-							</Col>
-						</Row>
+					<Col >
+						<div>
+						https://stackoverflow.com/questions/36862334/get-viewport-window-height-in-reactjs
+						</div>
+							
 					</Col>
 				</Row>
 			</Container>
+			<Navbar fixed="bottom">
+			    <Container>
+				    <InputGroup className="my-2">
+						<FormControl
+					      placeholder="Type your message..."
+					      aria-label="Type your message..."
+					      aria-describedby="basic-addon2"
+					    />
+					    <InputGroup.Append>
+					      <Button variant="outline-success">></Button>
+					    </InputGroup.Append>
+					</InputGroup>
+				</Container>
+			</Navbar>
 		</>
 	)
 }
