8fa90dc4e5e086f204541855655706b71b5b2228
LeonardoDM <leonardodiasmoraes@hotmail.com>
2021-01-25 17:59:41 -0300

    chat interface changed to card

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -1,50 +1,55 @@
 import React from 'react'
 import './styles.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Button, Container, Col, FormControl, InputGroup, Navbar, Row} from 'react-bootstrap'
+import {Button, Card, Container, Col, FormControl, InputGroup, Nav, Row} from 'react-bootstrap'
 
 export default function Chat() {
 	
 	return (
 		<>
-			<Navbar fixed="top" className="mb-2">
-			    <Container>
-				    <Navbar.Brand>
-				      <img
-				        alt=""
-				        src="/chat-4-128.png"
-				        width="35"
-				        height="35"
-				        className="d-inline-block align-top"
-				      />{' '}
-				      Chat App
-				    </Navbar.Brand>
-				</Container>
-			</Navbar>
-			<Container className="container-height">
+			<Container>
 				<Row>
-					<Col >
-						<div>
-						https://stackoverflow.com/questions/36862334/get-viewport-window-height-in-reactjs
-						</div>
-							
+					<Col>
+						<Card bg="light" border="light" className="card-height">
+							<Card.Header>
+								<Nav variant="tabs" defaultActiveKey="#index" className="nav-fill">
+							      <Nav.Item>
+							        <Nav.Link href="#index">
+							        	<img
+									        alt=""
+									        src="/chat-4-128.png"
+									        width="25"
+									        height="25"
+									      />{' '}
+									      Chat App
+							        </Nav.Link>
+							      </Nav.Item>
+							      <Nav.Item>
+							        <Nav.Link href="#users">Users Online</Nav.Link>
+							      </Nav.Item>
+							    </Nav>
+							</Card.Header>
+							<Card.Body className="chat-box">
+								<p>texto</p>
+								<p>texto</p>
+								<p>texto</p>
+							</Card.Body>
+							<Card.Footer>
+								<InputGroup className="my-2">
+									<FormControl
+								      placeholder="Type your message..."
+								      aria-label="Type your message..."
+								      aria-describedby="basic-addon2"
+								    />
+								    <InputGroup.Append>
+								      <Button variant="outline-success">></Button>
+								    </InputGroup.Append>
+								</InputGroup>
+							</Card.Footer>
+						</Card>
 					</Col>
 				</Row>
 			</Container>
-			<Navbar fixed="bottom">
-			    <Container>
-				    <InputGroup className="my-2">
-						<FormControl
-					      placeholder="Type your message..."
-					      aria-label="Type your message..."
-					      aria-describedby="basic-addon2"
-					    />
-					    <InputGroup.Append>
-					      <Button variant="outline-success">></Button>
-					    </InputGroup.Append>
-					</InputGroup>
-				</Container>
-			</Navbar>
 		</>
 	)
 }
